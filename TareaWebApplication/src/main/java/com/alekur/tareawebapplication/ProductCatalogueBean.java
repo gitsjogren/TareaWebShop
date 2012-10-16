fd4524eac3ab8deab48a53e97f8d95a807d02216
Alexander Kurkimäki <alekur@ed-3354-06.studat.chalmers.se>
2012-10-16 10:21:53 +0200

    Påbörjat arbete på projektet, körbart med lite funktionalitet

---
+++ TareaWebApplication/src/main/java/com/alekur/tareawebapplication/ProductCatalogueBean.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alekur.tareawebapplication;
+
+import com.eriksjo.tareawebshop.IProductCatalogue;
+import com.eriksjo.tareawebshop.JPAShop;
+import com.eriksjo.tareawebshop.Product;
+import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+
+/**
+ *
+ * @author alekur
+ */
+@SessionScoped
+public class ProductCatalogueBean implements Serializable{
+    IProductCatalogue pc = JPAShop.INSTANCE.getProductCatalogue();
+    private int i = 0;
+    
+    public void addP(){
+        if(i==0){
+        i = 1;
+        JPAShop.INSTANCE.getProductCatalogue().add(new Product(1L,"Produkt",13.37));
+        }
+    }
+    public List<Product> getAll(){
+        return JPAShop.INSTANCE.getProductCatalogue().getAll();
+    }
+    
+     public void add(Product p) {
+        pc.add(p);
+    }
+    
+    public Product find(Long id) {
+        return pc.find(id);
+    }
+
+   
+    public void update(Product p) {
+        pc.update(p);
+    }
+
+   
+    public void remove(Long id) {
+        pc.remove(id);
+    }
+    
+}
