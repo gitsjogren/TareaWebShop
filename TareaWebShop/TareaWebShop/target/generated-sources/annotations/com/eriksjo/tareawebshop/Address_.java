da05b20747e9d2242ad49f1837614c8b7baaed21
edfast <rickard.edfast@gmail.com>
2012-10-19 14:51:40 +0200

    Rensar login

---
+++ TareaWebShop/TareaWebShop/target/generated-sources/annotations/com/eriksjo/tareawebshop/Address_.java
@@ -0,0 +1,15 @@
+package com.eriksjo.tareawebshop;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2012-10-16T08:53:15")
+@StaticMetamodel(Address.class)
+public class Address_ { 
+
+    public static volatile SingularAttribute<Address, Integer> nbr;
+    public static volatile SingularAttribute<Address, String> street;
+    public static volatile SingularAttribute<Address, String> town;
+
+}
